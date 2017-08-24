fed573c2c21be345e0bc2b7987a88fe6c5cfffb5
FFly <f.f.l.y@hotmail.com>
2017-08-24 11:15:32 +0800

    添加微信程序、按钮

---
+++ pages/bluetooth/bluetooth.js
@@ -0,0 +1,19 @@
+Page({
+  data: {
+  },
+  getStatus: function (e) {
+  },
+  open: function (e) {
+  },
+  startScan: function (e) {
+  },
+  stopScan: function (e) {
+  },
+  getDevices: function (e) {
+  },
+  connect: function (e) {
+  },
+  close: function (e) {
+  }
+})
+
