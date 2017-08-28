31890ac50e203f32c78f1e47ccb2c56aa2ec2718
FFly <f.f.l.y@hotmail.com>
2017-08-28 12:12:57 +0800

    秀城cleartimeout的用法

--- pages/bluetooth/bluetooth.js
+++ pages/bluetooth/bluetooth.js
@@ -1,13 +1,24 @@
 var scanner = require('../../lib/bluetooth/scanner')
+var divices = [];
 
 Page({
   data: {
+    items: []
+  },
+  onLoad() {
   },
   getStatus: function (e) {
   },
   open: function (e) {
   },
   startScan: function (e) {
+    var that = this;
+
+    divices = [];
+    that.setData({
+      items: divices
+    })
+
     scanner.startScan({
       services: ['FEE7'],
       allowDuplicatesKey: false,
@@ -16,17 +27,25 @@ Page({
       closed: function (res) {
         console.log('APP closed', res);
       },
-      timeout:function(res){
+      timeout: function (res) {
         console.log('APP timeout', res);
+        console.log(divices.length);
+        that.setData({
+          items: divices
+        })
       },
-      failed:function(res){
+      failed: function (res) {
         console.log('APP failed', res);
       },
-      started:function(res){
+      started: function (res) {
         console.log('APP started', res);
       },
-      found:function(res){
-        console.log('APP found', res);
+      found: function (device) {
+        console.log('APP found', device);
+        divices.push(device);
+        that.setData({
+          items: divices
+        })
       }
     });
   },
